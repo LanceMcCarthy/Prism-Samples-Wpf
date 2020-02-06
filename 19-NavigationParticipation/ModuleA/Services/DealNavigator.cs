a41db16621e9a00a29b66bb3dc80e668da8765b9
Lance McCarthy <lance.mccarthy@progress.com>
2020-02-06 14:03:39 -0500

    Ticket 1451227 Solution
    NavigationParticipation - Added Telerik UI for WPF, reproduced UI thread issue in OnNavigatedTo, implemented solution.
    
    To observe, open and run the NavigationParticipation (#19) solution and click "Navigate to ViewB"

---
+++ 19-NavigationParticipation/ModuleA/Services/DealNavigator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ModuleA.Services
+{
+    public class DealNavigator
+    {
+        public ObservableCollection<string> MessageTexts { get; set; } = new ObservableCollection<string>();
+
+        public Task StartFillAsyncTask()
+        {
+            Debug.WriteLine($"Start Fill Async Task running on Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            // simulating long running work on this thread
+            Thread.Sleep(1500);
+
+            for (int i = 0; i < 500; i++)
+            {
+                this.MessageTexts.Add($"Line:{i}");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- 19-NavigationParticipation/ModuleA/ViewModels/ViewBViewModel.cs
+++ 19-NavigationParticipation/ModuleA/ViewModels/ViewBViewModel.cs
@@ -1,31 +1,61 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using ModuleA.Services;
+using Prism.Mvvm;
 using Prism.Regions;
 
 namespace ModuleA.ViewModels
 {
     public class ViewBViewModel : BindableBase, INavigationAware
     {
+        DealNavigator _Deal_Navigator_frmDataMgr;
         private string _title = "ViewB";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
-
         private int _pageViews;
-        public int PageViews
-        {
-            get { return _pageViews; }
-            set { SetProperty(ref _pageViews, value); }
-        }
+        private bool _isBusy;
 
         public ViewBViewModel()
         {
+            _Deal_Navigator_frmDataMgr = new DealNavigator();
+        }
 
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public int PageViews
+        {
+            get => _pageViews;
+            set => SetProperty(ref _pageViews, value);
+        }
+
+        public ObservableCollection<string> MessageTexts => _Deal_Navigator_frmDataMgr.MessageTexts;
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+        private async Task FillForm()
+        {
+            await _Deal_Navigator_frmDataMgr.StartFillAsyncTask();
+
+            // We are NOT on the UI thread, so this has to be marshaled back
+            Application.Current.Dispatcher?.Invoke(() =>
+            {
+                IsBusy = false;
+            });
         }
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
+            IsBusy = true;
+
+            Task.Run(FillForm);
+
             PageViews++;
         }
 
@@ -36,7 +66,6 @@ namespace ModuleA.ViewModels
 
         public void OnNavigatedFrom(NavigationContext navigationContext)
         {
-
         }
     }
 }
